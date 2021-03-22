0a58362065912d55546f2268bff9c057b8d994f8
shivaswaroop40 <shivaswaroop40@gmail.com>
2021-03-22 12:47:15 +0530

    This update is from branch 2

--- file.go
+++ file.go
@@ -8,5 +8,6 @@ func main() {
 	fmt.Printf("Hello world for the third time")
 	fmt.Printf("Hello world for the first time from new branch")
 	fmt.Printf("Hello world for the second time from new branch")
+	fmt.Printf("This is branched update")
 
 }
