990d5cd98200586a1acd7deafabfcf3460cec0de
shivaswaroop40 <shivaswaroop40@gmail.com>
2021-03-22 12:39:34 +0530

    this is second commit after changing branch

--- file.go
+++ file.go
@@ -7,5 +7,6 @@ func main() {
 	fmt.Printf("Hello world again")
 	fmt.Printf("Hello world for the third time")
 	fmt.Printf("Hello world for the first time from new branch")
+	fmt.Printf("Hello world for the second time from new branch")
 
 }
